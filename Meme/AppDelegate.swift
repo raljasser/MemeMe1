205d293e7d32d7abec0e35bec7f946b6758bf9fe
Rana Aljasser <r263j@hotmail.com>
2019-02-16 17:16:26 +0300

    1

--- Meme/AppDelegate.swift
+++ Meme/AppDelegate.swift
@@ -13,8 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var memes = [Meme]()
-
-    
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
